f5166e5b3899019a3a323c87e7964c44a30de8d9
FaizoKen <138367568+FaizoKen@users.noreply.github.com>
2025-03-08 23:47:10 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default ({ mode }) => {
       rollupOptions: {
         input: {
           main: resolve(__dirname, "index.html"),
-          // nested: resolve(__dirname, "nested/index.html"),
         },
       },
     },
