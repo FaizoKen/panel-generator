bcfa97f46274a0d8ce37a7b266efe265bd5a5587
FaizoKen <138367568+FaizoKen@users.noreply.github.com>
2025-03-09 17:12:03 +0800

    1

--- src/components/EditorComponentButtonShowModal.tsx
+++ src/components/EditorComponentButtonShowModal.tsx
@@ -27,12 +27,11 @@ export default function EditorComponentSelectMenuOption({
   modalIndex,
   modalId,
 }: Props) {
-    const modalCount = useCurrentMessageStore((state) => {
-        const modalData = state.getButton(rowIndex, compIndex);
-        if (!modalData || !("modals" in modalData)) return 0;
-        return modalData.modals.length;
-      });
-      
+  const modalCount = useCurrentMessageStore((state) => {
+    const modalData = state.getButton(rowIndex, compIndex);
+    if (!modalData || !("modals" in modalData)) return 0;
+    return modalData.modals.length;
+  });
 
   const [moveUp, moveDown, duplicate, remove] = useCurrentMessageStore(
     (state) => [
@@ -48,17 +47,13 @@ export default function EditorComponentSelectMenuOption({
     (state) => state.setSelectMenuOptionLabel
   );
 
-  const setName = useCurrentMessageStore(
-    (state) => state.setModalName
-  );
+  const setName = useCurrentMessageStore((state) => state.setModalName);
 
   const setPlaceholder = useCurrentMessageStore(
     (state) => state.setModaPlaceholder
   );
 
-  const setValue = useCurrentMessageStore(
-    (state) => state.setModalValue
-  );
+  const setValue = useCurrentMessageStore((state) => state.setModalValue);
 
   const setminLength = useCurrentMessageStore(
     (state) => state.setModalminLength
@@ -67,18 +62,18 @@ export default function EditorComponentSelectMenuOption({
     (state) => state.setModalmaxLength
   );
 
-    const [style, setStyle] = useCurrentMessageStore(
-      (state) => [
-        state.getModal(rowIndex, compIndex, modalIndex)?.style,
-        state.setModaStyle,
-      ],
-      shallow
-    );
+  const [style, setStyle] = useCurrentMessageStore(
+    (state) => [
+      state.getModal(rowIndex, compIndex, modalIndex)?.style,
+      state.setModaStyle,
+    ],
+    shallow
+  );
 
-      const [disabled, setRequired] = useCurrentMessageStore((state) => [
-        state.getModal(rowIndex, compIndex, modalIndex)?.required,
-        state.setModalRequired,
-      ]);
+  const [disabled, setRequired] = useCurrentMessageStore((state) => [
+    state.getModal(rowIndex, compIndex, modalIndex)?.required,
+    state.setModalRequired,
+  ]);
 
   const modal = useCurrentMessageStore(
     (state) => {
@@ -88,127 +83,135 @@ export default function EditorComponentSelectMenuOption({
     },
     shallow
   );
-  
-  
+
   if (!modal) {
     return <div></div>;
   }
 
-  return (
-<div className="border border-dark-6 rounded-lg p-4 bg-dark-8 shadow-md">
-  <Collapsable
-    id={`components.${rowId}.select.${compId}.modals.${modalId}`}
-    valiationPathPrefix={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}`}
-    title={`Modal ${modalIndex + 1}`}
-    extra={
-      modal.name && (
-        <div className="text-gray-500 truncate flex items-center space-x-2">
-          <span>-</span>
-          <span className="truncate">{modal.name}</span>
-        </div>
-      )
-    }
-    buttons={
-      <div className="flex items-center space-x-3">
-        {modalIndex > 0 && (
-          <ChevronUpIcon
-            className="h-6 w-6 text-gray-300 hover:text-blue-400 transition-colors cursor-pointer"
-            role="button"
-            onClick={() => moveUp(rowIndex, compIndex, modalIndex)}
-          />
-        )}
-        {modalIndex < modalCount - 1 && (
-          <ChevronDownIcon
-            className="h-6 w-6 text-gray-300 hover:text-blue-400 transition-colors cursor-pointer"
-            role="button"
-            onClick={() => moveDown(rowIndex, compIndex, modalIndex)}
-          />
-        )}
-        {modalCount < 25 && (
-          <DocumentDuplicateIcon
-            className="h-5 w-5 text-gray-300 hover:text-blue-400 transition-colors cursor-pointer"
-            role="button"
-            onClick={() => duplicate(rowIndex, compIndex, modalIndex)}
-          />
-        )}
-        <TrashIcon
-          className="h-5 w-5 text-red-400 hover:text-red-500 transition-colors cursor-pointer"
-          role="button"
-          onClick={() => remove(rowIndex, compIndex, modalIndex)}
-        />
-      </div>
-    }
-  >
-    <div className="space-y-4">
-      <EditorInput
-        label="Name"
-        maxLength={80}
-        value={modal.name}
-        onChange={(v) => setName(rowIndex, compIndex, modalIndex, v)}
-        className="flex-auto"
-        validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.name`}
-      />
-      <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
-        <div className="flex-1">
-          <label className="block text-gray-300 uppercase text-sm font-medium mb-1">
-            Required
-          </label>
-          <CheckBox
-            checked={disabled ?? false}
-            onChange={(v) => setRequired(rowIndex, compIndex, modalIndex, v)}
-          />
-        </div>
-        <div>
-        <label className="block text-gray-300 uppercase text-sm font-medium mb-1">
-          Style
-        </label>
-        <select
-          className="bg-dark-2 rounded-md p-2 w-full font-light cursor-pointer text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-          value={style?.toString()}
-          onChange={(v) => setStyle(rowIndex, compIndex, modalIndex, v.target.value)}
-        >
-          <option value="1">Short</option>
-          <option value="2">Paragraph</option>
-        </select>
-      </div>
-        <EditorInput
-          label="Min Length"
-          type="number"
-          value={modal.minLength?.toString() || ""}
-          onChange={(v) => setminLength(rowIndex, compIndex, modalIndex, Number(v))}
-          className="flex-1"
-          validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.minLength`}
-        />
-        <EditorInput
-          label="Max Length"
-          type="number"
-          value={modal.maxLength?.toString() || ""}
-          onChange={(v) => setmaxLength(rowIndex, compIndex, modalIndex, Number(v))}
-          className="flex-1"
-          validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.maxLength`}
-        />
-      </div>
-      <EditorInput
-        label="Placeholder"
-        type="textarea"
-        maxLength={80}
-        value={modal.placeholder}
-        onChange={(v) => setPlaceholder(rowIndex, compIndex, modalIndex, v)}
-        className="flex-auto"
-        validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.placeholder`}
-      />
-      <EditorInput
-        label="Value"
-        type="textarea"
-        maxLength={80}
-        value={modal.value}
-        onChange={(v) => setValue(rowIndex, compIndex, modalIndex, v)}
-        className="flex-auto"
-        validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.value`}
-      />
-    </div>
-  </Collapsable>
-</div>
+  const inputType = style === 2 ? "textarea" : "text";
 
+  return (
+    <div className="border border-dark-6 rounded-lg p-4 bg-dark-8 shadow-md">
+      <Collapsable
+        id={`components.${rowId}.select.${compId}.modals.${modalId}`}
+        valiationPathPrefix={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}`}
+        title={`Modal ${modalIndex + 1}`}
+        extra={
+          modal.name && (
+            <div className="text-gray-500 truncate flex items-center space-x-2">
+              <span>-</span>
+              <span className="truncate">{modal.name}</span>
+            </div>
+          )
+        }
+        buttons={
+          <div className="flex items-center space-x-3">
+            {modalIndex > 0 && (
+              <ChevronUpIcon
+                className="h-6 w-6 text-gray-300 hover:text-blue-400 transition-colors cursor-pointer"
+                role="button"
+                onClick={() => moveUp(rowIndex, compIndex, modalIndex)}
+              />
+            )}
+            {modalIndex < modalCount - 1 && (
+              <ChevronDownIcon
+                className="h-6 w-6 text-gray-300 hover:text-blue-400 transition-colors cursor-pointer"
+                role="button"
+                onClick={() => moveDown(rowIndex, compIndex, modalIndex)}
+              />
+            )}
+            {modalCount < 25 && (
+              <DocumentDuplicateIcon
+                className="h-5 w-5 text-gray-300 hover:text-blue-400 transition-colors cursor-pointer"
+                role="button"
+                onClick={() => duplicate(rowIndex, compIndex, modalIndex)}
+              />
+            )}
+            <TrashIcon
+              className="h-5 w-5 text-red-400 hover:text-red-500 transition-colors cursor-pointer"
+              role="button"
+              onClick={() => remove(rowIndex, compIndex, modalIndex)}
+            />
+          </div>
+        }
+      >
+        <div className="space-y-4">
+          <EditorInput
+            label="Name"
+            maxLength={80}
+            value={modal.name}
+            onChange={(v) => setName(rowIndex, compIndex, modalIndex, v)}
+            className="flex-auto"
+            validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.name`}
+          />
+          <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
+            <div className="flex-1">
+              <label className="block text-gray-300 uppercase text-sm font-medium mb-1">
+                Required
+              </label>
+              <CheckBox
+                checked={disabled ?? false}
+                onChange={(v) => setRequired(rowIndex, compIndex, modalIndex, v)}
+              />
+            </div>
+            <div>
+              <label className="block text-gray-300 uppercase text-sm font-medium mb-1">
+                Style
+              </label>
+              <select
+                className="bg-dark-2 rounded-md p-2 w-full font-light cursor-pointer text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value={style?.toString()}
+                onChange={(v) =>
+                  setStyle(rowIndex, compIndex, modalIndex, v.target.value)
+                }
+              >
+                <option value="1">Short</option>
+                <option value="2">Paragraph</option>
+              </select>
+            </div>
+            <EditorInput
+              label="Min Length"
+              type="number"
+              value={modal.minLength?.toString() || ""}
+              onChange={(v) =>
+                setminLength(rowIndex, compIndex, modalIndex, Number(v))
+              }
+              className="flex-1"
+              validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.minLength`}
+            />
+            <EditorInput
+              label="Max Length"
+              type="number"
+              value={modal.maxLength?.toString() || ""}
+              onChange={(v) =>
+                setmaxLength(rowIndex, compIndex, modalIndex, Number(v))
+              }
+              className="flex-1"
+              validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.maxLength`}
+            />
+          </div>
+          <EditorInput
+            label="Placeholder"
+            type={inputType}
+            maxLength={80}
+            value={modal.placeholder}
+            onChange={(v) =>
+              setPlaceholder(rowIndex, compIndex, modalIndex, v)
+            }
+            className="flex-auto"
+            validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.placeholder`}
+          />
+          <EditorInput
+            label="Value"
+            type={inputType}
+            maxLength={80}
+            value={modal.value}
+            onChange={(v) => setValue(rowIndex, compIndex, modalIndex, v)}
+            className="flex-auto"
+            validationPath={`components.${rowIndex}.components.${compIndex}.modals.${modalIndex}.value`}
+          />
+        </div>
+      </Collapsable>
+    </div>
   );
 }
