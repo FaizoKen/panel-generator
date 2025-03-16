41375aecc5ea4d9c619f77d06360e93ac8407e40
FaizoKen <138367568+FaizoKen@users.noreply.github.com>
2025-03-17 02:28:10 +0800

    1

--- src/views/editor/EncodedMessageURL.ts
+++ src/views/editor/EncodedMessageURL.ts
@@ -21,7 +21,7 @@ export const useShortLink = () => {
 
   useEffect(() => {
     if (encodedURL) {
-      fetch(`https://faizo.top?url=${encodedURL}`)
+      fetch(`https://faizo.top?data=${encodedURL.split('/').pop()}`)
         .then((res) => res.text())
         .then((shortened) => setShortURL(shortened))
         .catch((error) => console.error("Shortening failed:", error));
